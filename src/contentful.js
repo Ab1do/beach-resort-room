ca13f1ff877a27e0710e307dddc06e16f8ceb8ff
Abdallah <abdo.aa797@gmail.com>
2021-01-25 07:42:38 +0200

    edit .env

--- src/contentful.js
+++ src/contentful.js
@@ -3,4 +3,6 @@ import { createClient } from "contentful";
 export default createClient({
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  // space: "niabnclrne1p",
+  // accessToken: "JLt3gJVYDC3TnLIdgXtWyvTWZwfT4PhdnkhNMbudjlg",
 });
